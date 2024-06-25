29c32557d72ab78e14ab8101422133fbad4be407
divisha-t09 <89679781+divisha-t09@users.noreply.github.com>
2024-06-25 15:48:23 +0530

    Update Bank account details.cpp

--- Bank account details.cpp
+++ Bank account details.cpp
@@ -1,42 +1,76 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class account{
-
+class account {
 protected:
-      char name[25];
-      int accno;
-      char type;
-      int bal;
+    char name[25];
+    int accno;
+    char type;
+    int bal;
+
 public:
-     
-      void input(){
-        cout<<"Enter customer name(without space): ";cin>>name;
-        cout<<"Enter account number: ";cin>>accno;
+    void input() {
+        cout << "Enter customer name(without space): "; cin >> name;
+        cout << "Enter account number: "; cin >> accno;
         fflush(stdin);
-        cout<<"Enter type of account(Savings/Current Account): "; cin>>type;
+        cout << "Enter type of account (S/C for Savings/Current): "; cin >> type;
         fflush(stdin);
-        cout<<"Enter bal: ";cin>>bal;
-      }
-      void display(){
-            cout<<"\n Customer Name- "<<name;
-            cout<<"\n Account Number- "<<accno;
-            cout<<"\n Type of Account- "<<type;
-            cout<<"\n Balance- "<<bal;
-      }
-      void deposit(){
-            int amt;
-            cout<<"\n Enter the amount to deposit";
-            cin>>amt;
-            bal=bal+amt;
-      }
+        cout << "Enter balance: "; cin >> bal;
+    }
+
+    void display() {
+        cout << "\nCustomer Name: " << name;
+        cout << "\nAccount Number: " << accno;
+        cout << "\nType of Account: " << (type == 'S' ? "Savings" : "Current");
+        cout << "\nBalance: " << bal << endl;
+    }
+
+    void deposit() {
+        int amt;
+        cout << "Enter the amount to deposit: ";
+        cin >> amt;
+        bal += amt;
+        cout << "New balance: " << bal << endl;
+    }
+
+    void withdraw() {
+        int amt;
+        cout << "Enter the amount to withdraw: ";
+        cin >> amt;
+        if (amt > bal) {
+            cout << "Insufficient balance!" << endl;
+        } else {
+            bal -= amt;
+            cout << "New balance: " << bal << endl;
+        }
+    }
 };
 
-int main()
-{
-	account C1;
-	C1.input();
-	C1.display();
-	
-	return(0);
+int main() {
+    account C1;
+    C1.input();
+    C1.display();
+
+    int choice;
+    do {
+        cout << "Choose an action (1 -> deposit, 2 -> withdraw, 3 -> done): ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                C1.deposit();
+                break;
+            case 2:
+                C1.withdraw();
+                break;
+            case 3:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "Invalid choice, please try again." << endl;
+        }
+
+    } while (choice != 3);
+
+    return 0;
 }
